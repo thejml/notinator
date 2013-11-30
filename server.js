c5be923b1ddff395d374bc508c499c21f56d53c9
Joe Legeckis <thejml@gmail.com>
2013-11-29 21:59:00 -0500

    adding endpoints for notinator

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ var MONGOPORT = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
 var mongoose = require ("mongoose"); 
 var restify = require ("restify");
 var deploySchema = new mongoose.Schema({
-	release: { type: String, trim: true },
+	data: { type: String, trim: true },
 	datestamp: { type: Number, min: 0 },
-	md5: { type: String },
-	location: { type: String },
-	codebase: { type: String },
-	server: { type: String },
-	success: { type: Number },
-	environment: { type: String },
+	user: { type: String },
+	sharing: { type: Number },
 });
 // Get environment currently running under
 var env = "live";
@@ -22,7 +18,7 @@ var config = require('./config.js').configure(env);
 
 var deployment = mongoose.model('Deployments', deploySchema);
 	
-function respond(req, res, next) {
+function addNote(req, res, next) {
 	console.log(req.params);	
 	if (req.params.server === undefined) {
 	    return next(new restify.InvalidArgumentError('Server must be supplied'))
@@ -44,13 +40,14 @@ function respond(req, res, next) {
 
 	// Saving it to the database.
 	deployment.findOneAndUpdate({ server: req.params.server, release: req.params.release, codebase: req.params.codebase }, incomingDeployment, options, function (err) {
-		if (err) {console.log('Error on save'+err); res.send('Error on save!')} else { console.log('Saved!'); res.send('Saved!')}
+		if (err) {console.log('Error on save'+err);} else { console.log('Saved!');}
 	});
-  	res.send('hello ' + req.params.server);
+  	res.send('Thanks ' + req.params.server);
 }
 
-function findlatest(server) {
-	return deployment.aggregate({key: {"server":1},reduce: function (curr,result) {result.total++; if(curr.datestamp>result.datestamp) { result.datestamp=curr.datestamp;} },initial: {total:0, datestamp: 0} });
+function displayNote(req,res,next) {
+	res.send(req.params.nname+" thanks "+req.params.uname);
+//	return deployment.aggregate({key: {"server":1},reduce: function (curr,result) {result.total++; if(curr.datestamp>result.datestamp) { result.datestamp=curr.datestamp;} },initial: {total:0, datestamp: 0} });
 }
 
 function listLatestPerServer(req, res, next) {
@@ -59,27 +56,17 @@ function listLatestPerServer(req, res, next) {
 	deployment.find({ server: req.params.name }, null, { sort: { datestamp: -1 } },function(err, deploys) { res.send(deploys); });
 }
 
-function listLatestPerEnvironment(req, res, next) {
-	console.log("Quering..."+req.params.name);
-
-	deployment.find({ environment: req.params.name }, null, { sort: { datestamp: -1 }, limit: 1 },function(err, deploys) { res.send(deploys); });
-}
-
-function listLatestForAll(req, res, next) {
-// This will group by server and release. It will have multiple server entries because each server will have multiple releases.
-	//deployment.aggregate([{$project:{server: 1, release: 1, datestamp: 1}},{ $group: { _id: { server: "$server", release: "$release" },  mostRecent: { $max: "$datestamp" } } } ], 
-	deployment.aggregate([{$project:{server: 1, release: 1, datestamp: 1}}, { $group: { _id: { server: "$server" },  mostRecent: { $max: "$datestamp" } } } ], 
-		function(err, deploys) { res.send(deploys); });
-}
+function displayAllNotes(req,res,next) { }
+function deleteNote(req,res,next) { }
 
 var server = restify.createServer();
 server.use(restify.bodyParser());
-server.get('/latest/server/:name', listLatestPerServer);
-server.get('/latest/_all', listLatestForAll);
-server.get('/latest/_enviro/:name', listLatestPerEnvironment);
-server.get('/deploy/:name', respond);
-server.post('/deploy/:name', respond);
-server.head('/deploy/:name', respond);
+server.get('/note/_all', displayAllNotes);
+server.get('/note/:uname/:nname', displayNote);
+server.get('/delete/:uname', deleteNote);
+server.post('/add/:uname', addNote);
+server.post('/update/:uname', addNote);
+server.head('/note/:uname', addNote);
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
